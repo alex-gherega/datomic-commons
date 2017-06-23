27a6b3f912b4c3267553f0bff0400100eeb25ed2
alex-gherega <alex.gherega@gmail.com>
2017-06-23 11:00:27 +0300

    peer vs client

--- src/datomic_commons/db.clj
+++ src/datomic_commons/db.clj
@@ -1,9 +1,19 @@
 (ns datomic-commons.db
-  (:require [clojure.core.async :refer (<!!)] 
+  (:require [clojure.core.async :refer (<!!)]
+            [datomic.api :as d]
             [datomic.client :as client]))
 
-(defn connect [connection]
-  (<!! (client/connect connection)))
+(defn connect [connection-uri]
+  (d/connect connection-uri))
 
 (defn transact [conn schema]
+  (d/transact conn schema))
+
+(defn transact-async [conn schema]
+  (d/transact-async conn schema))
+
+(defn c-connect [connection]
+  (<!! (client/connect connection)))
+
+(defn c-transact [conn schema]
   (<!! (client/transact conn {:tx-data schema})))
